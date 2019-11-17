7569ddfec92b18224e4a8400f9009474b8a82b1a
saiprasad169 <33078446+saiprasad169@users.noreply.github.com>
2019-11-18 00:10:04 +0530

    Update ec2_snapshot.sh

--- ec2_snapshot.sh
+++ ec2_snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-playbook playbook.yml -e"add_snapshot=false del_snapshot=true"
+ansible-playbook playbook.yml -e"add_snapshot=true del_snapshot=true"
 
 # CRON job
 # 0 0 1 * * ansible-ec2-snapshot/ec2_snapshot.sh
